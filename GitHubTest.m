0477239b65f9b8251adfe6bf296f59de873ad7d7
DevonVail <44177767+DevonVail@users.noreply.github.com>
2018-10-15 16:15:20 -0400

    GitHubTest.m
    A MATLAB file. The purpose is to see if I can properly upload it to GitHub

---
+++ GitHubTest.m
@@ -0,0 +1,11 @@
+%Created by: DevonVail
+%Last updated: 10/15/2018
+%Description: This file is to test whether or not I can successfully upload
+%a file to GitHub
+clc; clear; 
+
+a = 5;
+b = 6;
+c = a*b;
+
+fprintf("%d \n",c)
